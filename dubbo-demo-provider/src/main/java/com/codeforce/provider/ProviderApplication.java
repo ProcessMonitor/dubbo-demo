fdbcba35775446d7f154a9633553f11adde335f6
ProcessMonitor <zhang19970321@qq.com>
2024-08-17 16:57:27 +0800

    2024年8月17日16:56:34 更改pom依赖

--- dubbo-demo-provider/src/main/java/com/codeforce/provider/ProviderApplication.java
+++ dubbo-demo-provider/src/main/java/com/codeforce/provider/ProviderApplication.java
@@ -1,7 +1,10 @@
 package com.codeforce.provider;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 public class ProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
